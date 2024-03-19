0f47e1bf1132a3ccc76b862371b7111e9e05f989
Maraxoni <maraxoni16@gmail.com>
2024-03-19 09:19:54 +0100

    Science works

--- CalculatorScience.cpp
+++ CalculatorScience.cpp
@@ -1,94 +1,142 @@
 #include <iostream>
-#include <cmath>
 
-int main() {
-    float x, y, result;
-    char operation;
+using namespace std;
 
-    std::cout << "Enter first number: ";
-    std::cin >> x;
+float calculate_science(float x, char op) {
+    float result;
 
-    std::cout << "Select operation (+, -, *, /, s - sin, c - cos, t - tan, n - ln, l - log10): ";
-    std::cin >> operation;
+    asm volatile (
+        "flds %1 \n"              // Load x onto FPU stack as a single-precision floating-point value
+        "movb %2, %%al \n"        // Move op to al register
 
-    if (operation != 's' && operation != 'c' && operation != 't' && operation != 'n' && operation != 'l') {
-        std::cout << "Enter second number: ";
-        std::cin >> y;
-    }
+        // Check operator, Trigonometric functions
+        "cmpb $'s', %%al \n"      // If 's' (sine) jump to sine
+        "je sine \n"
+        "cmpb $'c', %%al \n"      // If 'c' (cosine) jump to cosine
+        "je cosine \n"
+        "cmpb $'t', %%al \n"      // If 't' (tangent) jump to tangent
+        "je tangent \n"
 
-    asm (
-        "fld %[x]\n\t"
-        "cmp $'+', %[operation]\n\t"
-        "je addition\n\t"
-        "cmp $'-', %[operation]\n\t"
-        "je subtraction\n\t"
-        "cmp $'*', %[operation]\n\t"
-        "je multiplication\n\t"
-        "cmp $'/', %[operation]\n\t"
-        "je division\n\t"
-        "cmp $'s', %[operation]\n\t"
-        "je sin_func\n\t"
-        "cmp $'c', %[operation]\n\t"
-        "je cos_func\n\t"
-        "cmp $'t', %[operation]\n\t"
-        "je tan_func\n\t"
-        "cmp $'n', %[operation]\n\t"
-        "je ln_func\n\t"
-        "cmp $'l', %[operation]\n\t"
-        "je log10_func\n\t"
-        "jmp finish\n\t"
+        // Check operator, Logarithm
+        "cmpb $'l', %%al \n"      // If 'l' (logarithm) jump to logarithm
+        "je logarithm \n"
+        "cmpb $'n', %%al \n"      // If 'n' (natural logarithm) jump to natural_logarithm
+        "je natural_logarithm \n"
 
-        "addition:\n\t"
-        "fld %[y]\n\t"
-        "fadd\n\t"
-        "jmp finish\n\t"
+        "jmp endsc \n"
 
-        "subtraction:\n\t"
-        "fld %[y]\n\t"
-        "fsub\n\t"
-        "jmp finish\n\t"
+        // Sine
+        "sine: \n"
+        "fsin \n"                 // Compute sine
+        "fstps %0 \n"             // Store result from FPU stack to float value result
+        "jmp endsc \n"
 
-        "multiplication:\n\t"
-        "fld %[y]\n\t"
-        "fmul\n\t"
-        "jmp finish\n\t"
+        // Cosine
+        "cosine: \n"
+        "fcos \n"                 // Compute cosine
+        "fstps %0 \n"             // Store result from FPU stack to result as a single-precision floating-point value
+        "jmp endsc \n"
 
-        "division:\n\t"
-        "fld %[y]\n\t"
-        "fdiv\n\t"
-        "jmp finish\n\t"
+        // Tangent
+        "tangent: \n"
+        "fptan \n"                // Compute tangent
+        "fstp %%st(0) \n"         // Pop the unneeded value (1) from the FPU stack
+        "fstps %0 \n"             // Store result (tangent) from FPU stack to result as a single-precision floating-point value
+        "jmp endsc \n"
 
-        "sin_func:\n\t"
-        "fsin\n\t"
-        "jmp finish\n\t"
+        // Logarithm (base 10)
+        "logarithm: \n"
+        "fldlg2\n"            // Load log base 10 (log10(2)) onto FPU stack
+        "fld %1\n"            // Load x onto FPU stack
+        "fyl2x\n"             // Compute log base 10
+        "fstp %0\n"           // Store result from FPU stack to 'result'
+        "jmp endsc \n"
 
-        "cos_func:\n\t"
-        "fcos\n\t"
-        "jmp finish\n\t"
+        // Natural Logarithm (ln)
+        "natural_logarithm: \n"
+        "fldln2\n"            // Load natural log base 2 (ln(2)) onto FPU stack
+        "fld %1\n"            // Load x onto FPU stack
+        "fyl2x\n"             // Compute natural logarithm
+        "fstp %0\n"           // Store result from FPU stack to 'result'
+        "jmp endsc \n"
 
-        "tan_func:\n\t"
-        "fptan\n\t"
-        "fstp %%st(0)\n\t"  // Wyrzuć tangent, zostaw tylko wynik
-        "jmp finish\n\t"
-
-        "ln_func:\n\t"
-        "fldln2\n\t"
-        "fdiv\n\t"
-        "jmp finish\n\t"
-
-        "log10_func:\n\t"
-        "fldlg2\n\t"
-        "fdiv\n\t"
-        "jmp finish\n\t"
-
-        "finish:\n\t"
-        "fstp %[result]"
-        : [result] "=t" (result)
-        : [x] "m" (x), [y] "m" (y), [operation] "m" (operation)
-        : "memory"
+        "endsc: \n"
+        : "=m" (result)           // Output - result
+        : "m" (x), "g" (op)       // Input - x, op
+        : "eax"                   // EAX register used
     );
 
-    std::cout << "Result: " << result << std::endl;
+    return result;
+}
+
+float calculate(float x, float y, char op) {
+    float result;
+
+    asm volatile (
+        "movss %1, %%xmm0 \n"   // Move x to xmm0
+        "movss %2, %%xmm1 \n"   // Move y to xmm1
+
+        // Check operator
+        "cmp $'+', %3 \n"       // If '+' jump to addition
+        "je addition \n"
+        "cmp $'-', %3 \n"       // If '-' jump to subtraction
+        "je subtraction \n"
+        "cmp $'*', %3 \n"       // If '*' jump to multiplication
+        "je multiplication \n"
+        "cmp $'/', %3 \n"       // If '/' jump to division
+        "je division \n"
+        "cmp $'s', %3 \n"       // If 's' (sine) jump to sine
+
+        "addition: \n"
+        "addss %%xmm1, %%xmm0 \n" // Add xmm1 to xmm0
+        "jmp end \n"
+
+        "subtraction: \n"
+        "subss %%xmm1, %%xmm0 \n" // Subtract xmm1 from xmm0
+        "jmp end \n"
+
+        "multiplication: \n"
+        "mulss %%xmm1, %%xmm0 \n" // Multiply xmm0 by xmm1
+        "jmp end \n"
+
+        "division: \n"
+        "divss %%xmm1, %%xmm0 \n" // Divide xmm0 by xmm1
+        "jmp end \n"
+
+        "end: \n"
+        "movss %%xmm0, %0 \n"   // Move result from xmm0 to 'result'
+        : "=m" (result)         // Output - result
+        : "m" (x), "m" (y), "r" (op) // Input - x, y, op
+        : "xmm0", "xmm1"        // SSE registers xmm0 and xmm1 used in SSE instructions
+    );
+
+    return result;
+}
+
+int main() {
+    float num1, num2;
+    char operation;
+
+    cout << "Enter operation(+, -, *, /, s(sin), c(cos), t(tan), l(logarithm), n(natural logarithm)): ";
+    cin >> num1 >> operation;
+
+    if (cin && (operation == '+' || operation == '-' || operation == '*' || operation == '/')) {
+        cin >> num2;
+
+        float result = calculate(num1, num2, operation);
+
+        cout << "Result: " << result << "\n";
+    }
+    else if (operation == 's' || operation == 'c' || operation == 't' || operation == 'l' || operation == 'n') {
+        num2 = 0;
+
+        float result = calculate_science(num1, operation);
+
+        cout << "Result: " << result << "\n";
+    }
+    else {
+        cout << "Invalid input!" << endl;
+    }
 
     return 0;
 }
